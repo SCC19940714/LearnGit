5285441f2715e7ba3903d8933d75b24512ef8d78
sangchenchen <sangchenchen@glaway.com>
2022-04-08 16:00:20 +0800

    sss

---
+++ learngit/git-one/src/test/java/com/scc/gitone/GitOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scc.gitone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
