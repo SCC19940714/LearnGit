83db1a0320ca45d5ece3314631d53fae3d50ba3e
sangchenchen <sangchenchen@glaway.com>
2022-04-08 16:29:45 +0800

    ccc

---
+++ learngit/git-two/src/main/java/com/scc/gittwo/GitTwoApplication.java
@@ -0,0 +1,13 @@
+package com.scc.gittwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTwoApplication.class, args);
+	}
+
+}
